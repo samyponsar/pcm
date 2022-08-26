a49c20c562967e6e4a99d9db4778fe97e3f4c8a8
samyponsar <95660129+samyponsar@users.noreply.github.com>
2022-08-27 00:10:27 +0200

    mama
    added audio settings
    added class Audio & children Osc and Final
    moved stuff where stuff belongs
    added noise osc
    etc

--- pcm.cpp
+++ pcm.cpp
@@ -15,151 +15,49 @@
 
 using namespace std;
 
-const int CHANNELS = 1;
-const int SAMPLERATE = 44100;
-const int BITRATE = 16;
+static const int INT24_MAX = 8388607;
 
-vector<double> audioData; //final raw PCM data
+class Audio {
+public:
+	static int CHANNELS;
+	static int SAMPLERATE;
+	static int BITRATE;
+	vector<double> pcmData;
+	void set();
+}audio;
 
-class Osc{
+int Audio::CHANNELS = 1;
+int Audio::SAMPLERATE = 44100;
+int Audio::BITRATE = 24;
 
-		int mode = 0;
-		double duration = 1, frequency = 1, amplitude = 1;
-
-	public:
-		vector<double> oscData;
-
-		void init();
-		vector<double> sine();
-		vector<double> triangle();
-		vector<double> square();
-		vector<double> saw();
-		vector<double> sawr();
-		vector<double> silence();
-};
-
-void Osc::init(){
-	cout << "Osc mode? (1=sine | 2=triangle | 3=square | 4=saw | 5=reverse saw | 0=silence) ";
-	while ( !(cin >> mode) || !(mode == 0 || mode == 1 || mode == 2 || mode == 3 || mode == 4 || mode == 5) ) {
+void Audio::set() {
+	//cout << "Channels? (1=mono | 2=stereo) ";
+	//while (!(cin >> CHANNELS) || !(CHANNELS == 1 || CHANNELS == 2)) {
+	//	cin.clear();
+	//	cin.ignore(1000, '\n');
+	//	cout << "Invalid input.\nChannels? (1=mono | 2=stereo) ";
+	//}
+	cout << "Sample rate? (8000-352800 Hz) ";
+	while (!(cin >> SAMPLERATE) || SAMPLERATE < 8000 || SAMPLERATE > 352800) {
 		cin.clear();
 		cin.ignore(1000, '\n');
-		cout << "Invalid input.\nOsc mode? (1=sine | 2=triangle | 3=square | 4=saw | 5=reverse saw | 0=silence) ";
+		cout << "Invalid input.\nSample rate? (8000-352800 Hz) ";
 	}
-	cout << "Duration? (max. 3600 seconds) ";
-	while(!(cin >> duration) || duration < 0 || duration > 3600) {
+	cout << "Bit rate? (16 | 24) ";
+	while (!(cin >> BITRATE) || !(BITRATE == 16 || BITRATE == 24)) {
 		cin.clear();
 		cin.ignore(1000, '\n');
-		cout << "Invalid input.\nDuration? (max. 3600 seconds) ";
-	}
-	if (mode != 0) {
-		cout << "Frequency? (Hz) ";
-		while (!(cin >> frequency) || frequency <= 0) {
-			cin.clear();
-			cin.ignore(1000, '\n');
-			cout << "Invalid input.\nFrequency? (Hz) ";
-		}
-		if (frequency > SAMPLERATE / 2) {
-			cout << "WARNING: Input too high, aliasing will occur.\n";
-		}
-		cout << "Amplitude? (0-100, 1 = 0dBFS) ";
-		while (!(cin >> amplitude) || amplitude < 0 || amplitude > 100) {
-			cin.clear();
-			cin.ignore(1000, '\n');
-			cout << "Invalid input.\nAmplitude? (0-100, 1 = 0dBFS) ";
-		}
-		if (amplitude < 0.05) {
-			cout << "WARNING: Amplitude very low and dangerously close to noise floor.\n";
-		}
-	}
-	switch (mode) {
-		case 0:
-			oscData = silence();
-			break;
-		case 1:
-			oscData = sine();
-			break;
-		case 2:
-			oscData = triangle();
-			break;
-		case 3:
-			oscData = square();
-			break;
-		case 4:
-			oscData = saw();
-			break;
-		case 5:
-			oscData = sawr();
-			break;
+		cout << "Invalid input.\nBit rate? (16 | 24) ";
 	}
 }
 
-vector<double> Osc::silence() {
-	int length = (int)(duration * SAMPLERATE);
-	vector<double> result;
-	for (int x = 0; x < length; x++) {
-		double samplevalue = 0;
-		result.push_back(samplevalue);
-	}
-	return result;
-}
+class Final : public Audio {
+public:
+	void makewav();
+	void sum(Audio);
+}final;
 
-vector<double> Osc::sine() {
-	int length = (int) (duration * SAMPLERATE);
-	double period = (1/frequency) * SAMPLERATE;
-	vector<double> result;
-	for (int x = 0; x < length; x ++) {
-		double samplevalue = sin((x*2*M_PI)/period) * amplitude;
-		result.push_back(samplevalue);
-	}
-	return result;
-}
-
-vector<double> Osc::triangle() {
-	int length = (int) (duration * SAMPLERATE);
-	double period = (1/frequency) * SAMPLERATE;
-	vector<double> result;
-	for (int x = 0; x < length; x ++) {
-		double samplevalue = (4*amplitude/period)*abs(fmod((x+period-(period/4)),period)-(period/2))-amplitude;
-		result.push_back(samplevalue);
-	}
-	return result;
-}
-
-vector<double> Osc::square() {
-	int length = (int) (duration * SAMPLERATE);
-	double period = (1/frequency) * SAMPLERATE;
-	vector<double> result;
-	for (int x = 0; x < length; x ++) {
-		double samplevalue = sin((x*2*M_PI)/period);
-		samplevalue = ((samplevalue >= 0) - (samplevalue < 0)) * amplitude;
-		result.push_back(samplevalue);
-	}
-	return result;
-}
-
-vector<double> Osc::saw() {
-	int length = (int) (duration * SAMPLERATE);
-	double period = (1/frequency) * SAMPLERATE;
-	vector<double> result;
-	for (int x = 0; x < length; x ++) {
-		double samplevalue = (((fmod(x, period))/period)-0.5)*2*amplitude;
-		result.push_back(samplevalue);
-	}
-	return result;
-}
-
-vector<double> Osc::sawr() {
-	int length = (int) (duration * SAMPLERATE);
-	double period = (1/frequency) * SAMPLERATE;
-	vector<double> result;
-	for (int x = 0; x < length; x ++) {
-		double samplevalue = -(((fmod(x, period))/period)-0.5)*2*amplitude;
-		result.push_back(samplevalue);
-	}
-	return result;
-}
-
-int makewav(string filename) {
+void Final::makewav() {
 	typedef struct WAV_HEADER {
 		/* RIFF Chunk Descriptor */
 		uint8_t RIFF[4] = { 'R', 'I', 'F', 'F' }; // RIFF Header Magic header
@@ -170,48 +68,227 @@ int makewav(string filename) {
 		uint32_t Subchunk1Size = 16;           // Size of the fmt chunk
 		uint16_t AudioFormat = 1; // Audio format 1=PCM,6=mulaw,7=alaw,     257=IBM
 		// Mu-Law, 258=IBM A-Law, 259=ADPCM
-		uint16_t NumOfChan = CHANNELS;   // Number of channels 1=Mono 2=Stereo
-		uint32_t SamplesPerSec = SAMPLERATE;   // Sampling Frequency in Hz
-		uint32_t bytesPerSec = SAMPLERATE * (BITRATE / 8); // bytes per second
-		uint16_t blockAlign = CHANNELS * (BITRATE / 8);          // 2=16-bit mono, 4=16-bit stereo
-		uint16_t bitsPerSample = BITRATE;      // Number of bits per sample
+		uint16_t NumOfChan = (uint16_t) CHANNELS;   // Number of channels 1=Mono 2=Stereo
+		uint32_t SamplesPerSec = (uint32_t) SAMPLERATE;   // Sampling Frequency in Hz
+		uint32_t bytesPerSec = (uint32_t) (SAMPLERATE * (BITRATE / 8)); // bytes per second
+		uint16_t blockAlign = (uint16_t) (CHANNELS * (BITRATE / 8));          // 2=16-bit mono, 4=16-bit stereo
+		uint16_t bitsPerSample = (uint16_t) BITRATE;      // Number of bits per sample
 		/* "data" sub-chunk */
 		uint8_t Subchunk2ID[4] = { 'd', 'a', 't', 'a' }; // "data"  string
 		uint32_t Subchunk2Size = 0;                        // Sampled data length
 	} wav_hdr;
-	
-	uint32_t datasize = (uint32_t)(audioData.size()*(BITRATE/8));
+
+	string filename;
+	cout << "File name? ";
+	while (filename.length() == 0) {
+		getline(cin, filename);
+	}
+	while (filename.length() > 100 || !(regex_match(filename, regex("[\\w\\-. ]+")))) {
+		filename.clear();
+		cout << "Invalid input.\nFile name? ";
+		while (filename.length() == 0) {
+			getline(cin, filename);
+		}
+	}
+	filename += ".wav";
+
+	uint32_t datasize = (uint32_t)(pcmData.size() * (BITRATE / 8));
 	wav_hdr wav;
 	wav.ChunkSize = (datasize + sizeof(wav_hdr) - 8);
 	wav.Subchunk2Size = datasize;
 
 	ofstream out(filename, ios::binary);
-	
-	assert (sizeof(wav) == 44); //check header length
-	out.write((char *)&wav, sizeof(wav)); //write header
-	
-	for (double &i: audioData) {
-	  int16_t j = (int16_t)(clamp<double>(i,-1,1)*INT16_MAX); //map to 16bit range & preserve symmetry (min = -32767)
-	  out.write((char *)&j, sizeof(int16_t)); //write data
+
+	assert(sizeof(wav) == 44); //check header length
+	out.write((char*)&wav, sizeof(wav)); //write header
+
+	if (BITRATE == 16) {
+		for (double& i : pcmData) {
+			int16_t j = (int16_t)(clamp<double>(i, -1, 1) * INT16_MAX); //map to 16bit range & preserve symmetry (min = -32767)
+			out.write((char*)&j, sizeof(int16_t)); //write data
+		}
 	}
-	if (out.fail()){
+	else if (BITRATE == 24) {
+		for (double& i : pcmData) {
+			int32_t j = (int32_t)(clamp<double>(i, -1, 1) * INT24_MAX);	//map to 24bit range & preserve symmetry (min = -8388607)
+			out.write((char*)&j, sizeof(int8_t)*3); //write data
+		}
+	}
+
+
+
+	if (out.fail()) {
 		cout << "File write error!\n";
 	}
 	out.close();
-	return 0;
+}
+void Final::sum(Audio other) {
+	pcmData.resize(max(other.pcmData.size(), pcmData.size()));
+	transform(other.pcmData.begin(), other.pcmData.end(), pcmData.begin(), pcmData.begin(), plus<double>());
+}
+
+class Osc : public Audio {
+		int mode = 0, length = 0, preLength = 0;
+		double duration = 0, frequency = 1, amplitude = 0, preDelay = 0, period = 1;
+
+		vector<double> sine();
+		vector<double> triangle();
+		vector<double> square();
+		vector<double> saw();
+		vector<double> sawr();
+		vector<double> silence();
+		vector<double> noise();
+	public:
+		void set();
+}osc;
+
+void Osc::set(){
+	cout << "Osc mode? (1=sine | 2=triangle | 3=square | 4=saw | 5=reverse saw | 6=noise | 0=silence) ";
+	while ( !(cin >> mode) || mode < 0 || mode > 6) {
+		cin.clear();
+		cin.ignore(1000, '\n');
+		cout << "Invalid input.\nOsc mode? (1=sine | 2=triangle | 3=square | 4=saw | 5=reverse saw | 6=noise | 0=silence) ";
+	}
+	cout << "Duration? (max. 2000 seconds) ";
+	while(!(cin >> duration) || duration < 0 || duration > 2000) {
+		cin.clear();
+		cin.ignore(1000, '\n');
+		cout << "Invalid input.\nDuration? (max. 2000 seconds) ";
+	}
+	if (mode != 0) {
+		if (mode != 6) {
+			cout << "Frequency? (Hz) ";
+			while (!(cin >> frequency) || frequency <= 0) {
+				cin.clear();
+				cin.ignore(1000, '\n');
+				cout << "Invalid input.\nFrequency? (Hz) ";
+			}
+			if (frequency > SAMPLERATE / 2) {
+				cout << "WARNING: Input too high, aliasing will occur.\n";
+			}
+		}
+		cout << "Amplitude? (0-1000, %) ";
+		while (!(cin >> amplitude) || amplitude < 0 || amplitude > 1000) {
+			cin.clear();
+			cin.ignore(1000, '\n');
+			cout << "Invalid input.\nAmplitude? (0-1000, %) ";
+		}
+		if (amplitude < 5 && BITRATE == 16) {
+			cout << "WARNING: Amplitude very low and dangerously close to noise floor.\n";
+		}
+		amplitude = amplitude*0.01; //percentage to 0-1 value
+		cout << "Pre-delay? (max. 2000 seconds) ";
+		while (!(cin >> preDelay) || preDelay < 0 || preDelay > 2000) {
+			cin.clear();
+			cin.ignore(1000, '\n');
+			cout << "Invalid input.\nPre-delay? (max. 2000 seconds) ";
+		}
+	}
+
+	length = (int)(duration * SAMPLERATE);
+	preLength = (int)(preDelay * SAMPLERATE);
+	period = (1 / frequency) * SAMPLERATE;
+
+	switch (mode) {
+		case 0:
+			pcmData = silence();
+			break;
+		case 1:
+			pcmData = sine();
+			break;
+		case 2:
+			pcmData = triangle();
+			break;
+		case 3:
+			pcmData = square();
+			break;
+		case 4:
+			pcmData = saw();
+			break;
+		case 5:
+			pcmData = sawr();
+			break;
+		case 6:
+			pcmData = noise();
+			break;
+	}
+
+}
+
+vector<double> Osc::silence() {
+	vector<double> result(preLength, 0);
+	for (int x = 0; x < length; x++) {
+		double samplevalue = 0;
+		result.push_back(samplevalue);
+	}
+	return result;
+}
+
+vector<double> Osc::sine() {
+	vector<double> result(preLength, 0);
+	for (int x = 0; x < length; x ++) {
+		double samplevalue = sin((x*2*M_PI)/period) * amplitude;
+		result.push_back(samplevalue);
+	}
+	return result;
+}
+
+vector<double> Osc::triangle() {
+	vector<double> result(preLength, 0);
+	for (int x = 0; x < length; x ++) {
+		double samplevalue = (4*amplitude/period)*abs(fmod((x+period-(period*0.25)),period)-(period*0.5))-amplitude;
+		result.push_back(samplevalue);
+	}
+	return result;
+}
+
+vector<double> Osc::square() {
+	vector<double> result(preLength, 0);
+	for (int x = 0; x < length; x ++) {
+		double samplevalue = sin((x*2*M_PI)/period);
+		samplevalue = ((samplevalue >= 0) - (samplevalue < 0)) * amplitude;
+		result.push_back(samplevalue);
+	}
+	return result;
+}
+
+vector<double> Osc::saw() {
+	vector<double> result(preLength, 0);
+	for (int x = 0; x < length; x ++) {
+		double samplevalue = (((fmod(x, period))/period)-0.5)*2*amplitude;
+		result.push_back(samplevalue);
+	}
+	return result;
+}
+
+vector<double> Osc::sawr() {
+	vector<double> result(preLength, 0);
+	for (int x = 0; x < length; x ++) {
+		double samplevalue = -(((fmod(x, period))/period)-0.5)*2*amplitude;
+		result.push_back(samplevalue);
+	}
+	return result;
+}
+
+vector<double> Osc::noise() {
+	srand((int) time(nullptr));
+	vector<double> result(preLength, 0);
+	for (int x = 0; x < length; x++) {
+		double samplevalue = ((rand() / (double)(RAND_MAX*0.5))-1)*amplitude;
+		result.push_back(samplevalue);
+	}
+	return result;
 }
 
 int main() {
+	audio.set();
 	int oscCount = 0;
 	char another = 'y';
-	Osc osc;
 	while (another == 'y') {
 		oscCount ++;
 		cout << "Oscillator #" << oscCount << ':' << endl;
-		osc.init();
-		audioData.resize(max(audioData.size(), osc.oscData.size()));
-		//add osc data to the final audio data
-		transform(osc.oscData.begin(), osc.oscData.end(), audioData.begin(), audioData.begin(), plus<double>());
+		osc.set();
+		final.sum(osc);
 		cout << "Add another oscillator? (y/n) ";
 		while(!(cin >> another) || !(another == 'y' || another == 'n')) {
 			cin.clear();
@@ -219,20 +296,8 @@ int main() {
 			cout << "Invalid input.\nAdd another oscillator? (y/n) ";
 		}
 	}
-	string filename;
-	cout << "File name? ";
-	while (filename.length() == 0) {     
-	getline(cin, filename); 
-	}
-	while (filename.length() > 100 || !(regex_match(filename, regex("[\\w\\-. ]+")))) {
-		filename.clear();
-		cout << "Invalid input.\nFile name? ";
-		while (filename.length() == 0) {     
-		getline(cin, filename); 
-		}
-	}
-	filename += ".wav";
-	makewav(filename);
+
+	final.makewav();
 	
 	return 0;
 }
